1aca55917462e8914e7b945cc908c4e8db7ff41d
JeffBla <jeff920305@gmail.com>
2023-11-11 19:06:23 +0800

    :sparkles: (bone) update child after rotation & create an enum
    enum can identify the type of its object type.

--- common/Bone.cpp
+++ common/Bone.cpp
@@ -1,11 +1,35 @@
 #include "Bone.h"
+#include "Sphere.h"
+#include "ConvexMesh.h"
 
 using namespace bone;
 
-Bone::Bone(const std::string &bone_name,  PhysicsObject *bone_object,Bone *parent) : bone_name(bone_name), parent(parent),
-                                                                               bone_object(bone_object) {}
+Bone::Bone(const std::string &bone_name, PhysicsObject *bone_object, BoneType boneType, rp3d::Vector3 &pos,
+           Bone *parent)
+        : bone_name(bone_name), position(pos), parent(parent), bone_object(bone_object), boneType(boneType) {}
 
 Bone::~Bone() {
 
 }
 
+
+void Bone::UpdateChild() {
+    for (auto &[key, cBone]: children) {
+        rp3d::Quaternion quatern = cBone->GetPhysicsObject()->getTransform().getOrientation();
+        rp3d::Vector3 pos;
+        switch (boneType) {
+            case SPHERE:
+                pos = position + bone_object->getTransform().getOrientation() * default_orientation *
+                                 ((Sphere *) bone_object)->GetRadius();
+                break;
+            default:
+            case CONE:
+                pos = position + bone_object->getTransform().getOrientation() * default_orientation *
+                                 ((ConvexMesh *) bone_object)->GetSize().y;
+                break;
+        }
+
+        cBone->GetPhysicsObject()->setTransform(rp3d::Transform(pos, quatern));
+        cBone->UpdateChild();
+    }
+}
--- common/Bone.h
+++ common/Bone.h
@@ -8,25 +8,46 @@
 
 namespace bone {
 
+    enum BoneType{
+        CONE, SPHERE
+    };
+
     class Bone {
     private:
+        inline static const rp3d::Vector3 default_orientation{0,1,0};
+
+        BoneType boneType;
         std::string bone_name;
         PhysicsObject *bone_object;
+        rp3d::Vector3 position;
         Bone *parent;
         std::map<std::string, Bone *> children;
     public:
-        Bone(const std::string &bone_name, PhysicsObject *bone_object, Bone *parent);
+        Bone(const std::string &bone_name, PhysicsObject *bone_object, BoneType boneType, rp3d::Vector3 &pos, Bone *parent);
 
         ~Bone();
 
-        inline void AppendChild(Bone *child) {
-            children[child->bone_name] = child;
-        };
+        void AppendChild(Bone *child);
 
-        inline PhysicsObject *GetPhysicsObject(){
-            return bone_object;
-        };
+        void UpdateChild();
+
+        PhysicsObject *GetPhysicsObject();
+
+        rp3d::Vector3 &GetPosition();
     };
+
+    inline void Bone::AppendChild(Bone *child) {
+        children[child->bone_name] = child;
+    };
+
+    inline PhysicsObject *Bone::GetPhysicsObject() {
+        return bone_object;
+    };
+
+    inline rp3d::Vector3 &Bone::GetPosition() {
+        return position;
+    }
 }
+
 
 #endif //TESTBED_BONE_H
